c3f9ab75ebda29ba59bd347c0d15d1520b6b07cd
Dave Gabrielson <Dave.Gabrielson@umanitoba.ca>
2024-12-16 20:28:31 -0600

    Update to pytest

--- breakout_2/ATP_starter/python/gildedrose_starter/test_gilded_rose.py
+++ breakout_2/ATP_starter/python/gildedrose_starter/test_gilded_rose.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_needs_a_better_name(self):
-        # Arrange
-        items = [Item("foo", 0, 0)]
-        sut = GildedRose(items)
+def test_needs_a_better_name():
+    # Arrange
+    items = [Item("foo", 0, 0)]
+    sut = GildedRose(items)
 
-        # Act
-        sut.update_quality()
+    # Act
+    sut.update_quality()
 
-        # Assert
-        self.assertEquals("fixme", items[0].name)
+    # Assert
+    assert "fixme" == items[0].name
 
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    pytest.main()
