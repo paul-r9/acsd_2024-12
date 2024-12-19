7f9a18c1e67421b40cd15a1100b781ff76a33670
Sylvia Waldron <sylvia.waldron@umanitoba.ca>
2024-12-19 09:59:32 -0600

    Refactor gilded_rose.py

--- breakout_2/ATP_coveredgr/python/gilded_rose.py
+++ breakout_2/ATP_coveredgr/python/gilded_rose.py
@@ -21,45 +21,36 @@ class GildedRose(object):
 
     def update_single_item(self, item):
         if item.name == LEGENDARY_SULFURAS:
-            # legendary items don't update
+            # LEGENDARY_SULFURAS never changes in quality and we always want to sell it
             return
 
-        if item.name.startswith(CONJURED_PREFIX):
-            item.quality = min(50, max(0, item.quality - 2))
-            item.sell_in = item.sell_in - 1
-            # end conjured items
-            return
+        # By default, items decrease in quality over time
+        quality_update = -1
 
-        if (
-            item.name != "Aged Brie"
-            and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        ):
-            if item.quality > 0:
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
-        if item.name != "Sulfuras, Hand of Ragnaros":
-            item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if item.name != "Aged Brie":
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+        if item.name == BACKSTAGE_PASSES or item.name == AGED_BRIE:
+            # AGED_BRIE and BACKSTAGE_PASSES increase in quality over time
+            quality_update = 1
+        elif item.name.startswith(CONJURED_PREFIX):
+            # Conjured items decrease in quality twice as fast
+            quality_update = quality_update * 2
+
+        if item.name == BACKSTAGE_PASSES:
+            if item.sell_in <= 0:
+                # BACKSTAGE_PASSES are worthless after the concert
+                quality_update = -item.quality
+            elif item.sell_in <= FIVE_DAYS_OR_LESS:
+                # BACKSTAGE_PASSES get really valuable when concert is 5 days away
+                quality_update = quality_update * 3
+            elif item.sell_in <= TEN_DAYS_OR_LESS:
+                # BACKSTAGE_PASSES get more valuable when concert is 10 days away
+                quality_update = quality_update * 2
+        elif item.sell_in <= 0:
+            # AGED_BRIE, Conjured, and generic items have their quality change twice as fast after sell_in
+            quality_update = quality_update * 2
+
+        # Quality must be between 0 and 50
+        item.quality = max(0, min(50, item.quality + quality_update))
+        item.sell_in = item.sell_in - 1
 
 
 class Item:
@@ -69,4 +60,4 @@ class Item:
         self.quality = quality
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+        return f"{self.name}, {self.sell_in}, {self.quality}"
